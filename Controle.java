96775b51882dee97bdb61bd0b9a1d03077088c59
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-03-07 19:15:07 +0000

    controle

---
+++ Controle.java
@@ -0,0 +1,21 @@
+public static tableless{
+    public static void main(String[]args){
+        int valor = 0;
+        switch (valor) {
+            case 1:
+                system.out.println("valor = 1");
+                break;
+            case 2:
+                system.out.println("valor = 2");
+                break;
+            case 3:
+                system.out.println("valor = 3");
+                break;
+        
+            default:
+            System.out.prinln("Nenhum dos 3 valores")
+                
+        }
+    }
+
+}
